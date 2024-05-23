0a5b87c35cbc8e4cc52c573da669e2feaddf8ff3
wallysoneves <wallysoneves.moraes@gmail.com>
2024-05-23 17:12:58 -0300

    adicionando o swagger e liberando para ver os endpoints

--- src/main/java/br/com/abc/infrastructure/security/SecurityConfigurations.java
+++ src/main/java/br/com/abc/infrastructure/security/SecurityConfigurations.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
@@ -29,6 +31,7 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.GET, "/tasks/all").hasRole("ADMIN")
+                        .requestMatchers(antMatcher("/swagger-ui/index.html/**")).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
