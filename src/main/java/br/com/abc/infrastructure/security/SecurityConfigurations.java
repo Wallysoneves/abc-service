a024fd9b47ae700ed1477ee3b88b9f10e65a1a0c
wallysoneves <wallysoneves.moraes@gmail.com>
2024-06-05 18:12:46 -0300

    alteração no nome para inglês

--- src/main/java/br/com/abc/infrastructure/security/SecurityConfigurations.java
+++ src/main/java/br/com/abc/infrastructure/security/SecurityConfigurations.java
@@ -27,7 +27,7 @@ public class SecurityConfigurations {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/registrar").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.GET, "/task/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
